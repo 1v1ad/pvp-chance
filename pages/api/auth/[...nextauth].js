4ed64f067c7ad9f526fdf7f8568f14163399bd87
1v1ad <1v1ad@proton.me>
2025-07-28 09:16:49 +0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,14 @@ export default NextAuth({
     VKProvider({
       clientId: process.env.VK_CLIENT_ID,
       clientSecret: process.env.VK_CLIENT_SECRET,
+      authorization: {
+        url: "https://oauth.vk.com/authorize",
+        params: {
+          scope: "email",
+          response_type: "code",
+          redirect_uri: "https://pvp-chance.vercel.app/api/auth/callback/vk",
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
